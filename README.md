3caf2a64d5e21d793b3e5c77d8298cd08aa94bfb
SinekaRamu <sineka2001cud@gmail.com>
2023-09-13 23:56:56 +0530

    form submit input

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Recipe App
 
 Recipe App contains a form and card of the recipe. Form have a input element for title block, prepration time, steps for the recipe, image of the recipe and button to add the recipe as a card.
+
+## Form Submit
+
+Display the form input data in the console, while clicking submit
--- app/script.js
+++ app/script.js
@@ -1,17 +1,64 @@
 //Inialize the global vaiable
 const cardBody = document.querySelector("#display-card");
 
+let recipeItems = [];
+
 const recipeCount = document.querySelector("#count");
 recipeCount.innerText = "cardCount";
 console.log("print");
 
+// Main function
 updateForm();
+getFromLocalSorage();
 
 function updateForm() {
   const form = document.querySelector("#recipe-form");
   form.addEventListener("submit", function (r) {
     r.preventDefault();
-    // createId();
-    console.log(sumbit);
+    updateFormElements();
+    console.log("sumbit");
   });
+}
+
+function updateFormElements() {
+  // fetch form data
+  const title = document.querySelector("#recipeName").value;
+  const time = document.querySelector("#timeTaken").value;
+  const steps = document.querySelector("#stepsTodo").value;
+  const image = document.querySelector("#recipeImg").value;
+
+  const recipe = {
+    id: new Date().getTime(),
+    title: title,
+    time: time,
+    steps: steps,
+    image: image,
+  };
+  addRecipe(recipe);
+  console.log(recipe);
+}
+
+//function to add recipe
+function addRecipe(recipe) {
+  recipeItems.push(recipe);
+  setToLocalStorage();
+}
+
+// function to save data in local storage
+function setToLocalStorage() {
+  const str = JSON.stringify(recipeItems);
+  localStorage.setItem("Recipes", str);
+  //   updateMovieUI();
+}
+
+// Function to get data from localStorage
+function getFromLocalSorage() {
+  const str = localStorage.getItem("Recipes");
+  if (!str) {
+    recipeItems = [];
+  } else {
+    recipeItems = JSON.parse(str);
+  }
+  // updateMovieUI();
+  console.log(recipeItems);
 }
